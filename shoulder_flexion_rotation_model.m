b7eeb474ae13bcd535ed09d94152a274362c4911
qihuoxford <qi.hu@st-hildas.ox.ac.uk>
2016-03-27 11:59:48 +0800

    Update shoulder_flexion_rotation_model.m

--- shoulder_flexion_rotation_model.m
+++ shoulder_flexion_rotation_model.m
@@ -1,7 +1,18 @@
-function [v,a,angle] = shoulder_flexion_rotation_model(step_t,invtime)
-var = 0.0163;
-mean = 0.0008;
-gain = 55000;
+function [v,a,angle] = shoulder_flexion_rotation_model(step_t,invtime,choice)
+
+if choice ==1
+    var = 0.0163;
+    mean = 0.0008;
+    gain = 55000;
+elseif choice ==2
+    var = 0.22;
+    mean = 0;
+    gain = 305;
+elseif choice == 3
+    var = 5;
+    mean = 0;
+    gain = 0.6;
+end
 
 t = 0:step_t:invtime;
 v = gain*exp(-(t-mean).^2/(2*var)).*t.^3;
@@ -10,7 +21,7 @@ a = gain*exp(-(t-mean).^2/(2*var)).*t.^3.*...
 angle = zeros(1,length(t));
 
 for n = 2:length(t)
-angle(n) = angle(n-1)+v(n)*step_t;
+    angle(n) = angle(n-1)+v(n)*step_t;
 end
 
 figure
@@ -28,5 +39,7 @@ title('flexion angle')
 xlabel('time:s')
 ylabel('degree')
 
+
+end
 
 end
